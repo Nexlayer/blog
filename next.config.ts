8730f820012a8d0a25df451919c721862cec3056
Amishmaqbool <amishmaqbool@gmail.com>
2025-08-31 01:34:07 +0500

    Next config file updated

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,20 @@
 import { NextConfig } from 'next'
 import createMDX from '@next/mdx'
-
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   output: 'standalone',
   basePath: '/blog',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/blog',
+        permanent: true,
+      },
+    ]
+  },
 }
-
 const withMDX = createMDX({
   extension: /\.mdx?$/,
 })
-
 export default withMDX(nextConfig)
