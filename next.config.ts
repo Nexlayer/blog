0cba6e4650d69383d7ab5ef1e9b836485f252c25
Katie Harris <kharris@katies-mac-mini.mynetworksettings.com>
2025-09-01 15:27:18 -0400

    Updates

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import { NextConfig } from 'next'
 import createMDX from '@next/mdx'
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  output: 'standalone',
-  distDir: 'build',
+  output: 'export', // Enable static export
+  distDir: 'build', // Output directory
   basePath: '/blog'
 }
 const withMDX = createMDX({
