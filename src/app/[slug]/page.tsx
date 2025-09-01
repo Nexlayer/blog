b0c51a63fc55b753f0ab5ee578351ad6cd48e6f0
Katie Harris <kharris@katies-mac-mini.mynetworksettings.com>
2025-09-01 15:52:44 -0400

    Move around pages

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import SimpleMdxRenderer from "../../../components/SimpleMdxRenderer";
+import SimpleMdxRenderer from "@/components/SimpleMdxRenderer";
 import fs from "fs";
 import { ArrowLeft } from "lucide-react";
 import path from "path";
